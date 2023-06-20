fe1c5b0c32fb5f5292c0193352a2767d0bcd3047
alanjoelschneider <alanjoelschneider@hotmail.com>
2023-06-20 16:19:58 -0300

    First version

---
+++ rollup.config.mjs
@@ -0,0 +1,57 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import autoPreprocess from 'svelte-preprocess';
+import svelte from 'rollup-plugin-svelte';
+import terser from '@rollup/plugin-terser';
+import { spawn } from 'child_process';
+
+const production = !process.env.ROLLUP_WATCH;
+
+function typeCheck() {
+  let proc;
+
+  function toExit() {
+    if (proc) proc.kill(0);
+  }
+
+  return {
+    writeBundle() {
+      if (proc) return;
+
+      spawn('svelte-check', {
+        stdio: ['ignore', 'inherit', 'inherit'],
+        shell: true
+      });
+
+      process.on("SIGTERM", toExit);
+      process.on("exit", toExit);
+    }
+  }
+}
+
+export default {
+  input: './src/main.js',
+  output: {
+    name: 'app',
+    sourcemap: !production,
+    format: 'iife',
+    file: './dist/bundle.js',
+  },
+  plugins: [
+    typeCheck(),
+    resolve({
+      browser: true,
+      dedupe: ['svelte'],
+    }),
+    commonjs(),
+    svelte({
+      preprocess: autoPreprocess(),
+      compilerOptions: {
+        dev: !production,
+      },
+    }),
+    typescript({ sourceMap: !production }),
+    production && terser(),
+  ],
+};
---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  const title: string = 'Svelte project template!';
+</script>
+
+<h1>{title}</h1>
